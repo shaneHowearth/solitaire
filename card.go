0150ad930fddeca355bb52bfeeea348e5f087928
Shane Howearth <Shane.Howearth@codeworx.com.au>
2025-05-24 13:10:09 +1000

    Create cards, their values and suits

---
+++ card.go
@@ -0,0 +1,74 @@
+package solitaire
+
+// SuitCount - the number of suits.
+const SuitCount = 4
+
+// CardCount - the number of unsuited cards.
+const CardCount = 12
+
+// Card - Enumeration of cards.
+type Card int
+
+//nolint:revive // ignore need to comment this block of exported consts.
+const (
+	Ace Card = iota
+	Two
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+)
+
+func (card Card) String() string {
+	return [...]string{
+		"A",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10",
+		"J",
+		"Q",
+		"K",
+	}[card]
+}
+
+// Suit - Enumeration of suits.
+type Suit int
+
+//nolint:revive // ignore need to comment this block of exported consts.
+const (
+	Heart Suit = iota
+	Club
+	Diamond
+	Spade
+)
+
+func (suit Suit) String() string {
+	return [...]string{
+		"♥",
+		"♣",
+		"♦",
+		"♠",
+	}[suit]
+}
+
+// SuitedCard - A type that encapsulates that a hard has a rank/name and a
+// suit.
+//
+//nolint:unused // The fields are used elsewhere.
+type SuitedCard struct {
+	Card Card
+	Suit Suit
+}
