77a61feaa516b3da24ef0ab1c5cd447e764cd7c8
Shane Howearth <shane@shanehowearth.com>
2025-05-28 09:52:58 +1000

    Create a stack

---
+++ stack.go
@@ -0,0 +1,21 @@
+package solitaire
+
+// Stack (or Pile)
+// A stack of cards one on top of the other and squared such that only the topmost card,
+// whether face up or face down is visible.[5]
+type Stack []SuitedCard
+
+// There are two types of stack, the one that holds the reserve of cards that
+// are yet to be played, and the ones that are on the tableau.
+
+// Add - add a suited card to the stack.
+func (stack *Stack) Add(card SuitedCard, visible bool) {
+	card.Visible = visible
+
+	*stack = append(*stack, card)
+}
+
+// Len - return the length of the stack.
+func (stack *Stack) Len() int {
+	return len((*stack))
+}
