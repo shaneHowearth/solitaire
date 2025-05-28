c86973eb5b94957fa8ad93c6fa34cb86cac2098a
Shane Howearth <shane@shanehowearth.com>
2025-05-28 10:39:48 +1000

    Create tableau type

---
+++ tableau.go
@@ -0,0 +1,5 @@
+package solitaire
+
+// Tableau - An arrangement of cards on the table, typically comprising several
+// depots i.e. places where columns of overlapping cards may be formed.
+type Tableau []Stack
