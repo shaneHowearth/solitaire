a06d7af2d5e0ef82983eae1a2f7ea779f9df68d3
Shane Howearth <shane@shanehowearth.com>
2025-05-28 10:03:27 +1000

    refactored deck to use plural suits

--- deck.go
+++ deck.go
@@ -9,19 +9,19 @@ import (
 type Deck []SuitedCard
 
 var deck = []SuitedCard{
-	{Ace, Spade}, {Ace, Heart}, {Ace, Club}, {Ace, Diamond},
-	{Two, Spade}, {Two, Heart}, {Two, Club}, {Two, Diamond},
-	{Three, Spade}, {Three, Heart}, {Three, Club}, {Three, Diamond},
-	{Four, Spade}, {Four, Heart}, {Four, Club}, {Four, Diamond},
-	{Five, Spade}, {Five, Heart}, {Five, Club}, {Five, Diamond},
-	{Six, Spade}, {Six, Heart}, {Six, Club}, {Six, Diamond},
-	{Seven, Spade}, {Seven, Heart}, {Seven, Club}, {Seven, Diamond},
-	{Eight, Spade}, {Eight, Heart}, {Eight, Club}, {Eight, Diamond},
-	{Nine, Spade}, {Nine, Heart}, {Nine, Club}, {Nine, Diamond},
-	{Ten, Spade}, {Ten, Heart}, {Ten, Club}, {Ten, Diamond},
-	{Jack, Spade}, {Jack, Heart}, {Jack, Club}, {Jack, Diamond},
-	{Queen, Spade}, {Queen, Heart}, {Queen, Club}, {Queen, Diamond},
-	{King, Spade}, {King, Heart}, {King, Club}, {King, Diamond},
+	{Card: Ace, Suit: Spades}, {Card: Ace, Suit: Hearts}, {Card: Ace, Suit: Clubs}, {Card: Ace, Suit: Diamonds},
+	{Card: Two, Suit: Spades}, {Card: Two, Suit: Hearts}, {Card: Two, Suit: Clubs}, {Card: Two, Suit: Diamonds},
+	{Card: Three, Suit: Spades}, {Card: Three, Suit: Hearts}, {Card: Three, Suit: Clubs}, {Card: Three, Suit: Diamonds},
+	{Card: Four, Suit: Spades}, {Card: Four, Suit: Hearts}, {Card: Four, Suit: Clubs}, {Card: Four, Suit: Diamonds},
+	{Card: Five, Suit: Spades}, {Card: Five, Suit: Hearts}, {Card: Five, Suit: Clubs}, {Card: Five, Suit: Diamonds},
+	{Card: Six, Suit: Spades}, {Card: Six, Suit: Hearts}, {Card: Six, Suit: Clubs}, {Card: Six, Suit: Diamonds},
+	{Card: Seven, Suit: Spades}, {Card: Seven, Suit: Hearts}, {Card: Seven, Suit: Clubs}, {Card: Seven, Suit: Diamonds},
+	{Card: Eight, Suit: Spades}, {Card: Eight, Suit: Hearts}, {Card: Eight, Suit: Clubs}, {Card: Eight, Suit: Diamonds},
+	{Card: Nine, Suit: Spades}, {Card: Nine, Suit: Hearts}, {Card: Nine, Suit: Clubs}, {Card: Nine, Suit: Diamonds},
+	{Card: Ten, Suit: Spades}, {Card: Ten, Suit: Hearts}, {Card: Ten, Suit: Clubs}, {Card: Ten, Suit: Diamonds},
+	{Card: Jack, Suit: Spades}, {Card: Jack, Suit: Hearts}, {Card: Jack, Suit: Clubs}, {Card: Jack, Suit: Diamonds},
+	{Card: Queen, Suit: Spades}, {Card: Queen, Suit: Hearts}, {Card: Queen, Suit: Clubs}, {Card: Queen, Suit: Diamonds},
+	{Card: King, Suit: Spades}, {Card: King, Suit: Hearts}, {Card: King, Suit: Clubs}, {Card: King, Suit: Diamonds},
 }
 
 // CreateDecks - Create a deck of cards.
@@ -31,6 +31,7 @@ func CreateDecks(n int) *Deck {
 	}
 
 	d := Deck(slices.Repeat(deck, n))
+
 	return &d
 }
 
@@ -54,6 +55,8 @@ func (usedDeck *Deck) Shuffle() {
 func (usedDeck *Deck) Deal() SuitedCard {
 	if usedDeck.Len() == 0 {
 		// Empty, shouldn't be here.
+		// TODO: better to send an error back.
+		return SuitedCard{}
 	}
 
 	card := (*usedDeck)[(usedDeck.Len() - 1)]
@@ -61,4 +64,9 @@ func (usedDeck *Deck) Deal() SuitedCard {
 	*usedDeck = slices.Delete(*usedDeck, usedDeck.Len()-1, usedDeck.Len())
 
 	return card
+}
+
+// Top - Length of the Deck.
+func (usedDeck *Deck) Top() SuitedCard {
+	return (*usedDeck)[usedDeck.Len()-1]
 }
