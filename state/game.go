e35fe37ad842345ba0ae175212234ad55c6fb294
Shane Howearth <shane@shanehowearth.com>
2025-06-25 07:23:18 +1000

    Add a struct that holds all of the types required for a game to run

---
+++ state/game.go
@@ -0,0 +1,44 @@
+package state
+
+// State -
+type State struct {
+	Deck        *Deck
+	Tableau     []*Tableau
+	Talon       *Talon
+	Foundations []Foundation
+}
+
+// New - Create a new set of stacks.
+func New(
+	// Number of decks.
+	decks int,
+	// Foundation Setup.
+	numFoundations int,
+	foundationBase Rank,
+	foundationRule func(Foundation, SuitedCard) bool,
+	// Tableau Setup.
+	numTableau int,
+	tableauRule func(*Tableau, SuitedCard) bool,
+	// Talon Setup.
+	dealCount int,
+	perDealCount int,
+	talonRule func(SuitedCard) bool,
+) *State {
+	return &State{
+		Deck: CreateDecks(decks),
+		Foundations: CreateFoundations(
+			numFoundations,
+			foundationBase,
+			foundationRule,
+		),
+		Tableau: CreateTableaus(
+			numTableau,
+			tableauRule,
+		),
+		Talon: NewTalon(
+			dealCount,
+			perDealCount,
+			talonRule,
+		),
+	}
+}
