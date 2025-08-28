d70d2ca69fa9a784eabdd50d5aa6a60694d561c6
Shane Howearth <shane@shanehowearth.com>
2025-08-28 19:10:30 +1000

    Create gaps game

---
+++ game/gaps.go
@@ -0,0 +1,160 @@
+package game
+
+import "github.com/shanehowearth/solitaire/state"
+
+// Gaps - https://en.wikipedia.org/wiki/Gaps
+type Gaps struct{}
+
+// Ensure that Gaps implements game.Variant.
+var _ Variant = (*Gaps)(nil)
+
+// Name - name of the variant.
+func (*Gaps) Name() string {
+	return "Gaps"
+}
+
+const gapsColumns = 13
+
+// TableauGridSize - The size of the grid required by acme.
+func (*Gaps) TableauGridSize() (int, int) {
+	const rows = 4
+	const gapsColumns = 13
+
+	return rows, gapsColumns
+}
+
+// Decks - How many decks of cards are required to play acme.
+func (*Gaps) Decks() int {
+	return 1
+}
+
+// Reserves - how the reserves are defined.
+func (*Gaps) Reserves() []state.StackSpec {
+	// There are no reserves.
+	return []state.StackSpec{}
+}
+
+// Tableau - how the tableau are defined.
+func (gaps *Gaps) Tableau() []state.StackSpec {
+	// There are 52 tableau, each has one card that is open.
+	tableau := make([]state.StackSpec, 0, 52)
+	for i := 0; i < 52; i++ {
+		t := state.StackSpec{
+			AddRule:   gaps.tableauRule,
+			CardCount: [2]int{1, 1},
+			SkipCards: map[state.SuitedCard]struct{}{
+				state.SuitedCard{Rank: state.Ace, Suit: state.Hearts}:   struct{}{},
+				state.SuitedCard{Rank: state.Ace, Suit: state.Diamonds}: struct{}{},
+				state.SuitedCard{Rank: state.Ace, Suit: state.Clubs}:    struct{}{},
+				state.SuitedCard{Rank: state.Ace, Suit: state.Spades}:   struct{}{},
+			},
+		}
+		tableau = append(tableau, t)
+	}
+	return tableau
+}
+
+func (*Gaps) tableauRule(tableau *state.Stack, card state.SuitedCard) bool {
+	// Handle when the tableau is empty.
+	if tableau.Len() == 0 {
+		// Anything can be put onto an empty tableau.
+		return true
+	}
+
+	// All other cases the card should not be added to the tableau.
+	return false
+}
+
+// Foundations - how the foundations are defined.
+func (*Gaps) Foundations() []state.StackSpec {
+	// There are no foundations.
+	return []state.StackSpec{}
+}
+
+// HowToPlay - Tell the player how to play the game.
+func (*Gaps) HowToPlay() []string {
+	//nolint:lll // Long line needed to preserve readability of game rules text.
+	lines := []string{
+		`The cards are dealt into four rows of thirteen. The aces are removed and discarded from further play. The gaps that they leave behind are filled by cards that are the same suit and a rank higher than the card on the left of the gap. (For example, 4♣ can be placed beside 3♣.) However, any gap at the right of a King is considered dead and no card can fill it.
+`,
+
+		`Any gap on the left hand side of the row should be placed by a deuce and the row should be built up by suit beside the deuce (i. e. 2-3-4-5, etc.). It is the discretion of the player on which suit would occupy which row.
+`,
+		`When there are no more possible moves, the round is over. The cards that are not in order are gathered, making sure to leave any suit sequence (e.g., ♥2-3-4-5) behind. In some variations, the cards are then shuffled;[1][2] in others, they are not. The cards are then redealt, making sure there is a gap in each row at the immediate right of each suit sequence or at the extreme left of the row if no suit sequence is formed in that row. There are only three rounds in the usual rules. But some variants allow four, and some have no limit.
+`,
+		`The game is won when all 48 cards are arranged in numerical order and in suits, with the gaps of each row beside the Kings at the extreme right hand of the row.
+`,
+		`There is only two (2) redeal available in this game.`,
+	}
+
+	return lines
+}
+
+// HasWon - How to tell if the game has been won.
+func (*Gaps) HasWon(tableau []*state.Tableau, _ []*state.Foundation) bool {
+	if tableau[51].Stack.Len() < 2 {
+		return false
+	}
+	return true
+}
+
+// MaxRedeals - how many redeals are allowed.
+func (*Gaps) MaxRedeals() int {
+	// Only one redeal is allowed.
+	return 1
+}
+
+// Move -
+func (*Gaps) Move(source, destination *state.Stack, tableau []*state.Tableau) bool {
+	// Nothing to move.
+	if source.Len() == 0 {
+		return false
+	}
+
+	// Only one card per tableau.
+	if destination.Len() > 0 {
+		return false
+	}
+
+	sourceTop, err := source.Top()
+	if err != nil {
+		return false
+	}
+
+	if destination.TableauPosition%gapsColumns != 0 {
+		neighbourTop, err := tableau[destination.TableauPosition-1].Top()
+		if err != nil {
+			return false
+		}
+
+		// Nothing can be placed to the right of a King.
+		if neighbourTop.Rank == state.King {
+			return false
+		}
+
+		// Can only place a card of the same suit as the the card to the left of
+		// the gap.
+		if sourceTop.Suit != neighbourTop.Suit {
+			return false
+		}
+
+		// Card being moved must have a higher rank than the neighbour.
+		if neighbourTop.Rank > sourceTop.Rank {
+			return false
+		}
+	}
+
+	// Move the card.
+	destination.Add(sourceTop, true)
+	_, _ = source.Deal()
+
+	return true
+}
+
+// Compact
+func (*Gaps) Compact(_, _ *state.Stack, _ []*state.Tableau) {}
+
+// Talon
+func (*Gaps) Talon() bool {
+	return false
+}
