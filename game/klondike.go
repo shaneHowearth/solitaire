44cd4e4bfcd99497531502be698afba02db7e52e
Shane Howearth <shane@shanehowearth.com>
2025-10-16 05:33:54 +1000

    refactor checkMove to accept a boolean indicating if the sequence order matters in a stack

--- game/klondike.go
+++ game/klondike.go
@@ -175,6 +175,7 @@ func (*Klondike) AvailableMoves(tableau []state.Tableau, foundations []state.Fou
 				foundations[foundationIdx].Stack,
 				fmt.Sprintf("Tableau %d", sourceIdx+1),
 				fmt.Sprintf("Foundation %d", foundationIdx+1),
+				false,
 			); hint != "" {
 				hints = append(hints, hint)
 			}
@@ -186,6 +187,7 @@ func (*Klondike) AvailableMoves(tableau []state.Tableau, foundations []state.Fou
 			foundations[foundationIdx].Stack,
 			fmt.Sprintf("Waste"),
 			fmt.Sprintf("Foundation %d", foundationIdx+1),
+			false,
 		); hint != "" {
 			hints = append(hints, hint)
 		}
@@ -202,6 +204,7 @@ func (*Klondike) AvailableMoves(tableau []state.Tableau, foundations []state.Fou
 				tableau[destIdx].Stack,
 				fmt.Sprintf("Tableau %d", sourceIdx+1),
 				fmt.Sprintf("Tableau %d", destIdx+1),
+				false,
 			); hint != "" {
 				hints = append(hints, hint)
 			}
@@ -213,6 +216,7 @@ func (*Klondike) AvailableMoves(tableau []state.Tableau, foundations []state.Fou
 			tableau[destIdx].Stack,
 			fmt.Sprintf("Waste"),
 			fmt.Sprintf("Tableau %d", destIdx+1),
+			false,
 		); hint != "" {
 			hints = append(hints, hint)
 		}
