3431b79393e016eca5c6d5da71999261df8210a5
Shane Howearth <shane@shanehowearth.com>
2025-12-29 08:52:01 +1100

    Allow for games that have zero foundations

--- state/foundation.go
+++ state/foundation.go
@@ -16,11 +16,6 @@ type Foundation struct {
 
 // CreateFoundations - Create the foundations that will host the cards.
 func CreateFoundations(foundationSpec []StackSpec) []*Foundation {
-	// Check for zero.
-	if len(foundationSpec) == 0 {
-		panic("Cannot have zero foundations")
-	}
-
 	if len(foundationSpec)%SuitCount != 0 {
 		panic("Number of foundations must be a multiple of the number of suits in a deck")
 	}
