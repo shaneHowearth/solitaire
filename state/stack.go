1ab33ee6362e0f07a795e13cab023b4453d2a153
Shane Howearth <shane@shanehowearth.com>
2025-12-30 14:04:23 +1100

    feat: add FoundationPosition tracking to Stack
    Adds FoundationPosition field to Stack alongside existing TableauPosition.
    This enables pretty-printing foundation locations in hint display.

--- state/stack.go
+++ state/stack.go
@@ -8,16 +8,17 @@ import (
 // Stack - A stack of cards one on top of the other and squared such that only.
 // the topmost card, whether face up or face down is visible.[5].
 type Stack struct {
-	cards           *[]SuitedCard
-	Base            SuitedCard
-	Rule            func(SuitedCard) bool
-	ruleFactory     func(*Stack) func(SuitedCard) bool
-	Type            StackType
-	Received        int // count how many times this stack has received cards.
-	MaxRedeals      int
-	TableauPosition int // Simple count to determine where in the Tableau this is.
-	SkipCards       map[SuitedCard]struct{}
-	ShowCount       int // Number of cards to be shown in the stack.
+	cards              *[]SuitedCard
+	Base               SuitedCard
+	Rule               func(SuitedCard) bool
+	ruleFactory        func(*Stack) func(SuitedCard) bool
+	Type               StackType
+	Received           int // count how many times this stack has received cards.
+	MaxRedeals         int
+	TableauPosition    int // Simple count to determine where in the Tableau this is.
+	FoundationPosition int // Simple count to determine where in the Foundation this is.
+	SkipCards          map[SuitedCard]struct{}
+	ShowCount          int // Number of cards to be shown in the stack.
 }
 
 // StackType represents the type of game component.
